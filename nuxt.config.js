79b3d68a42b0c0bb397170b7cf4d79c4786738bb
Infant Frontender <newbornfrontender@gmail.com>
2018-09-25 11:56:07 +0300

    Добавил конфигурацию и включил shadow mode для vue-loader

---
+++ nuxt.config.js
@@ -0,0 +1,43 @@
+export default {
+  head: {
+    meta: [{
+      charset: 'utf-8',
+    }, {
+      name: 'viewport',
+      content: 'width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes',
+    }, {
+      'http-equiv': 'x-ua-compatible',
+      content: 'ie=edge',
+    }],
+  },
+
+  router: {
+    linkActiveClass: 'route-link--active',
+    linkExactActiveClass: 'route-link--active-exact',
+  },
+
+  loaders: {
+    vue: {
+      shadowMode: true,
+    },
+  },
+
+  build: {
+    postcss: {
+      preset: {
+        stage: 0,
+
+        browsers: 'last 2 versions',
+
+        autoprefixer: {
+          // grid: process.env.NODE_ENV === 'production' ? true : false,
+          grid: true,
+        },
+      },
+    },
+  },
+
+  loading: {
+    color: 'green',
+  },
+};
