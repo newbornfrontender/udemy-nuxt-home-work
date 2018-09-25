f921cebc834bbfee451870852bdf714fa1950f0e
Infant Frontender <newbornfrontender@gmail.com>
2018-09-25 14:46:19 +0300

    Вывел заголовок

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
-<template></template>
+<template>
+  <h1>{{ msg }}</h1>
+</template>
 
 <script>
   export default {
     name: 'index',
+
+    data: () => ({
+      msg: 'Hello world!',
+    }),
   };
 </script>
