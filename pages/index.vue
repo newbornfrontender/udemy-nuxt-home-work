d74db79abf9ff5713af41c45f63d9a00ec6f8e9b
Infant Frontender <newbornfrontender@gmail.com>
2018-09-25 11:57:25 +0300

    Добавил главную страницу

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <h1>{{ title }}</h1>
+  </section>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      title: 'Title',
+    }),
+  };
+</script>
+
+<style>
+  h1 {
+    color: orange;
+  }
+</style>
