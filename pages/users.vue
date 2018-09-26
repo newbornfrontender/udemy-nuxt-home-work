e811827d739186a567e2d382c004838b3e044e16
Infant Frontender <newbornfrontender@gmail.com>
2018-09-26 15:18:15 +0300

    Добавлена страница для примера с вложенным маршрутом

---
+++ pages/users.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input
+      type="text"
+      v-model="userId"
+    >
+
+    <button v-on:click="onLoadUsers">Load User</button>
+
+    <nuxt-child></nuxt-child>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'users',
+
+    data: () => ({
+      userId: '',
+    }),
+
+    methods: {
+      onLoadUsers () {
+        this.$router.push(`/users/${this.userId}`);
+      },
+    },
+  };
+</script>
