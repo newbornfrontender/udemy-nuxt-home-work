b526ea72bccf10ad6d82ac27612b4bb758cd8c98
Infant Frontender <newbornfrontender@gmail.com>
2018-09-26 15:18:44 +0300

    Перенесен код в users.vue и добавлен p

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,26 +1,3 @@
 <template>
-  <div>
-    <input
-      type="text"
-      v-model="userId"
-    >
-
-    <button v-on:click="onLoadUsers">Load User</button>
-  </div>
+  <p>Please enter an ID and click the button</p>
 </template>
-
-<script>
-  export default {
-    name: 'users',
-
-    data: () => ({
-      userId: '',
-    }),
-
-    methods: {
-      onLoadUsers () {
-        this.$router.push(`/users/${this.userId}`);
-      },
-    },
-  };
-</script>
