8e0d4373f795aae3d7723405da688e013679b4de
Infant Frontender <newbornfrontender@gmail.com>
2018-09-28 13:30:08 +0300

    Добавил макет для users страницы

---
+++ layouts/users.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <header>USERS SECTION</header>
+
+    <main>
+      <nuxt />
+    </main>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'layout-default',
+  };
+</script>
