dd63c4c93f62e19d8910cfbdbd7e692bdc0ab063
Infant Frontender <newbornfrontender@gmail.com>
2018-10-02 15:25:16 +0300

    Добавил статьи

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,128 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+
+    <section class="featured-posts">
+      <nuxt-link
+        class="post-preview"
+        v-bind:to="`/posts/${ + 1 }`"
+      >
+        <article>
+          <div
+            class="post-thumbnail"
+            style="background-image: url('https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e5a31d03ddee66863a599421f792e07b&w=1000&q=80');"
+          />
+
+          <div class="post-content">
+            <h1>Post Title 1</h1>
+
+            <p>Preview Text 1</p>
+          </div>
+        </article>
+      </nuxt-link>
+
+      <nuxt-link
+        class="post-preview"
+        v-bind:to="`/posts/${ + 2 }`"
+      >
+        <article>
+          <div
+            class="post-thumbnail"
+            style="background-image: url('https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e5a31d03ddee66863a599421f792e07b&w=1000&q=80');"
+          />
+
+          <div class="post-content">
+            <h1>Post Title 2</h1>
+
+            <p>Preview Text 2</p>
+          </div>
+        </article>
+      </nuxt-link>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'page-index',
-
-  data: () => ({
-    msg: '123',
-  }),
 };
 </script>
+
+<style scoped>
+.intro {
+  height: 300px;
+  position: relative;
+  padding: 30px;
+  box-sizing: border-box;
+  background-position: center;
+  background-size: cover;
+}
+
+.intro h1 {
+  position: absolute;
+  top: 10%;
+  left: 5%;
+  width: 90%;
+  font-size: 1.5rem;
+  color: black;
+  background-color: rgb(211, 211, 211);
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px black;
+  box-sizing: border-box;
+  border: 1px solid black;
+}
+
+@media (min-width: 768px) {
+  .intro h1 {
+    font-size: 2rem;
+  }
+}
+
+.featured-posts {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>
