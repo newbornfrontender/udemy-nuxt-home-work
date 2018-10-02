c2dc74470f55efb1c4a05b70cd78806d29e6597e
Infant Frontender <newbornfrontender@gmail.com>
2018-10-02 14:49:11 +0300

    Добавил классы и стили

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="single-posts-page">
     <section class="post">
-      <h1>Title of the Post</h1>
+      <h1 class="post-title">Title of the Post</h1>
 
       <div class="post-details">
-        <div>Last updated XXX</div>
+        <div class="post-detail">Last updated XXX</div>
 
-        <div>Writed by NAME</div>
+        <div class="post-detail">Writed by NAME</div>
       </div>
 
-      <p>Content of the post</p>
+      <p class="post-content">Content of the post</p>
     </section>
 
     <section class="post-feedback">
@@ -19,4 +19,55 @@
 </template>
 
 <style scope>
+.single-post-page {
+  padding: 30px;
+  text-align: center;
+  box-sizing: border-box;
+}
+
+.post {
+  width: 100%;
+}
+
+@media (min-width: 768px) {
+  .post {
+    width: 600px;
+    margin: auto;
+  }
+}
+
+.post-title {
+  margin: 0;
+}
+
+.post-details {
+  padding: 10px;
+  box-sizing: border-box;
+  border-bottom: 3px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 768px) {
+  .post-details {
+    flex-direction: row;
+  }
+}
+
+.post-detail {
+  color: rgb(88, 88, 88);
+  margin: 0 10px;
+}
+
+.post-feedback a {
+  color: red;
+  text-decoration: none;
+}
+
+.post-feedback a:hover,
+.post-feedback a:active {
+  color: salmon;
+}
 </style>
