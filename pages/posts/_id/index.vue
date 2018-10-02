22c85f75ed86f7ae0c8859756387caabc0faa99d
Infant Frontender <newbornfrontender@gmail.com>
2018-10-02 14:38:35 +0300

    Добавил динамические посты

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="single-posts-page">
+    <section class="post">
+      <h1>Title of the Post</h1>
+
+      <div class="post-details">
+        <div>Last updated XXX</div>
+
+        <div>Writed by NAME</div>
+      </div>
+
+      <p>Content of the post</p>
+    </section>
+
+    <section class="post-feedback">
+      <p>Let me know what you think about the post, send a mail to <a href="mailto:feedback@my-awesome-domain.com">feedback@my-awesome-domain.com</a>.</p>
+    </section>
+  </div>
+</template>
+
+<style scope>
+</style>
