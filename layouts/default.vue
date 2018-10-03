102b3dd9b85111bd158c8e307da1aeed2ecaa43e
Infant Frontender <newbornfrontender@gmail.com>
2018-10-03 16:52:54 +0300

    Перенес блок со стилями, добавил компоненты с боковым меню

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,3 @@
-<template>
-  <div>
-    <nuxt />
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'layout-default',
-};
-</script>
-
 <style>
 html {
   font-family: Open Sans, sans-serif;
@@ -19,3 +7,34 @@ body {
   margin: 0;
 }
 </style>
+
+<template>
+  <div>
+    <TheHeader v-on:sidenavToggle="displaySidenav = !displaySidenav" />
+
+    <TheSidenav
+      v-bind:show="displaySidenav"
+      v-on:close="displaySidenav = false"
+    />
+
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import TheHeader from '@/components/Navigation/TheHeader';
+import TheSidenav from '@/components/Navigation/TheSidenav';
+
+export default {
+  name: 'layout-default',
+
+  components: {
+    TheHeader,
+    TheSidenav,
+  },
+
+  data: () => ({
+    displaySidenav: false,
+  }),
+};
+</script>
