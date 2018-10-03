b0802ab3e9ae6f77f659435eeaebe8d88284f631
Infant Frontender <newbornfrontender@gmail.com>
2018-10-03 15:49:13 +0300

    Добавил компонент с постами

---
+++ components/Posts/PostPreview.vue
@@ -0,0 +1,63 @@
+<style scoped>
+.post-preview {
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #ccc;
+  background-color: white;
+  width: 90%;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+@media (min-width: 850px) {
+  .post-preview {
+    width: 400px;
+    margin: 10px;
+  }
+}
+
+.post-thumbnail {
+  width: 100%;
+  height: 200px;
+  background-position: center;
+  background-size: cover;
+}
+
+.post-content {
+  padding: 10px;
+  text-align: center;
+}
+
+a:hover .post-content,
+a:active .post-content {
+  background-color: #ccc;
+}
+</style>
+
+<template>
+  <nuxt-link
+    class="post-preview"
+    v-bind:to="`/posts/${ + 1 }`"
+  >
+    <article>
+      <div
+        class="post-thumbnail"
+        style="background-image: url('https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e5a31d03ddee66863a599421f792e07b&w=1000&q=80');"
+      />
+
+      <div class="post-content">
+        <h1>Post Title 1</h1>
+
+        <p>Preview Text 1</p>
+      </div>
+    </article>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'PostPreview',
+};
+</script>
