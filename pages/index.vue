b8dbda53713506314ad7a3752dd2ad34a9439e41
Infant Frontender <newbornfrontender@gmail.com>
2018-10-03 15:48:47 +0300

    Добавил имя для удобства отладки и перенес стили на верх. Переместил часть кода в компонент

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,3 @@
-<template>
-  <div class="home-page">
-    <section class="intro">
-      <h1>Get the latest tech news!</h1>
-    </section>
-
-    <section class="featured-posts">
-      <nuxt-link
-        class="post-preview"
-        v-bind:to="`/posts/${ + 1 }`"
-      >
-        <article>
-          <div
-            class="post-thumbnail"
-            style="background-image: url('https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e5a31d03ddee66863a599421f792e07b&w=1000&q=80');"
-          />
-
-          <div class="post-content">
-            <h1>Post Title 1</h1>
-
-            <p>Preview Text 1</p>
-          </div>
-        </article>
-      </nuxt-link>
-
-      <nuxt-link
-        class="post-preview"
-        v-bind:to="`/posts/${ + 2 }`"
-      >
-        <article>
-          <div
-            class="post-thumbnail"
-            style="background-image: url('https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e5a31d03ddee66863a599421f792e07b&w=1000&q=80');"
-          />
-
-          <div class="post-content">
-            <h1>Post Title 2</h1>
-
-            <p>Preview Text 2</p>
-          </div>
-        </article>
-      </nuxt-link>
-    </section>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'page-index',
-};
-</script>
-
 <style scoped>
 .intro {
   height: 300px;
@@ -89,40 +37,20 @@ export default {
   align-items: center;
   justify-content: center;
 }
-
-.post-preview {
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 2px #ccc;
-  background-color: white;
-  width: 90%;
-}
-
-a {
-  text-decoration: none;
-  color: black;
-}
-
-@media (min-width: 850px) {
-  .post-preview {
-    width: 400px;
-    margin: 10px;
-  }
-}
-
-.post-thumbnail {
-  width: 100%;
-  height: 200px;
-  background-position: center;
-  background-size: cover;
-}
-
-.post-content {
-  padding: 10px;
-  text-align: center;
-}
-
-a:hover .post-content,
-a:active .post-content {
-  background-color: #ccc;
-}
 </style>
+
+<template>
+  <div class="home-page">
+    <section class="intro">
+      <h1>Get the latest tech news!</h1>
+    </section>
+
+    <section class="featured-posts"></section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'page-index',
+};
+</script>
